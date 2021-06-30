d001f461e889b02b08da658d20337202c42a7ddc
VladimirM1ronov <mironowvs@yandex.ru>
2021-06-30 15:30:53 +0300

    Д17

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,24 @@
 const findHotelButton = document.querySelector(".searching-hotel-button");
 const popupHotel = document.querySelector(".modal");
+const form = popupHotel.querySelector(".searching-form");
+const dateArrival = popupHotel.querySelector(".modal-date-arrival");
+const dateDeparture = popupHotel.querySelector(".modal-date-departure");
+const adultNumber = popupHotel.querySelector(".adult-number");
+const childrenNumber = popupHotel.querySelector(".children-number");
 
-console.log("Pososi");
 
 findHotelButton.addEventListener("click", function() {
-	popupHotel.classList.toggle("modal-show")
+	popupHotel.classList.toggle("modal-show");
+	popupHotel.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function(evt) {
+	if ((!dateArrival.value || !dateDeparture.value) || (!adultNumber.value && !childrenNumber.value)) {
+		// console.log("False");
+		popupHotel.classList.add("modal-error");
+		evt.preventDefault();
+	} else {
+		// console.log("true");
+		evt.preventDefault();
+	}
 });
